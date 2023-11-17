40354e4fd4d7ddc37f4fe21c6619b97761658027
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2023-11-17 20:09:21 +0000

    Corrected the allowed hosts in settings as it pointed to the wrong place.

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = "django-insecure-6zci-xx4e#_%69a3p9edpqy4k316&8*so%gwuvqu9dm3b*8ac8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://school-management-system-proj-afcbd9b9e318.herokuapp.com', '.herokuapp.com/']
+ALLOWED_HOSTS = ['school-management-system-proj-afcbd9b9e318.herokuapp.com', '.herokuapp.com/']
 
 
 # Application definition
