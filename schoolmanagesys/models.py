14c568421bb51ac781626c3dc5f0d4091f8bc660
Charlotte <charlie.stone649@gmail.com>
2023-11-20 09:23:11 +0000

    Updated the models section to reflect courses, students and teachers.

--- schoolmanagesys/models.py
+++ schoolmanagesys/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Course(models.Model):
+    course_choices = (
+        (1, "Mathematics"),
+        (2, "Physics"),
+        (3, "Chemistry"),
+        (4, "Biology"),
+    )
+    
+    def __str__(self):
+        return self.name
+
+class Student(models.Model):
+    first_name = models.TextField(max_length=50)
+    last_name = models.TextField(max_length=50)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
+    courses = models.ManyToManyField(Course, related_name='students')
+    
+    can_see_grades = models.BooleanField(default=True)
+    can_see_attendance = models.BooleanField(default=True)
+    assigned_classes = models.ManyToManyField(Course, related_name='teachers')
+    course_choice = models.IntegerField(choices=Course.course_choices, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
+
+
+class Teacher(models.Model):
+    first_name = models.TextField(max_length=50)
+    last_name = models.TextField(max_length=50)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    can_assign_classes = models.BooleanField(default=True)
+    can_assign_courses = models.BooleanField(default=True)
+    can_edit_student_attendance = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
+
